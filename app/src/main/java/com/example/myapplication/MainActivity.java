91af0e40b22dfa299ebd2c04124281d96670bb04
Junsuk Oh <a811219@gmail.com>
2019-03-11 10:49:40 +0900

    어쩌구 수정함

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void helloButtonClicked(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("name", "오준석");
+        intent.putExtra("name", "장혁재");
         startActivity(intent);
     }
 }
